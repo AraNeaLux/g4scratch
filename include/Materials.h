ea757964ec7cb28acc30378d26df79d66b8359f3
pcbend <pcbend@gmail.com>
2023-03-15 12:07:12 -0400

    added materials class singleton skeleton

---
+++ include/Materials.h
@@ -0,0 +1,19 @@
+#ifndef __MATERIALS_H__
+#define __MATERIALS_H__
+
+class Materials {
+  private:
+    static Materials *fMaterials 
+    Materials::Materials();
+
+  public:
+    static Materials::Get(); 
+    Materials::~Materials();
+
+  private:
+
+};
+
+
+#endif
+
